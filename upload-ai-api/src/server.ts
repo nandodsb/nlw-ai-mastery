a228670bc43bdc5a1ead0be444ecefac56679f67
nandodsb <nandosbx@gmail.com>
2023-10-01 14:53:43 -0300

    Update

--- upload-ai-api/src/server.ts
+++ upload-ai-api/src/server.ts
@@ -9,7 +9,7 @@ import { generateAICompletionRoute } from './routes/generate-ai-completion';
 const app = fastify();
 
 app.register(fastifyCors, {
-    origin: '*'
+    origin: 'https://nlw-ai-mastery-sage.vercel.app'
 })
 
 app.register(getAllPromptsRoute)
