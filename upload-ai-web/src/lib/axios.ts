3cc422c258644bd322491c82a97ba27a141b004c
nandodsb <nandosbx@gmail.com>
2023-10-01 20:31:03 -0300

    Axios update

--- upload-ai-web/src/lib/axios.ts
+++ upload-ai-web/src/lib/axios.ts
@@ -1,8 +1,5 @@
 import axios from 'axios'
 
 export const api = axios.create({
-    baseURL: `${import.meta.env.VITE_APP_API_URL}`,
-    headers: {
-        "Content-type": "application/json",
-      },
+    baseURL: "http://localhost:3344",   
 })
