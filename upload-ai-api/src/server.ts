8b27d10779db4e230d58cc5ecd80b61fbe0801d2
nandodsb <nandosbx@gmail.com>
2023-10-02 00:01:40 -0300

    Prisma Migrate

--- upload-ai-api/src/server.ts
+++ upload-ai-api/src/server.ts
@@ -5,7 +5,6 @@ import { uploadVideoRoute } from './routes/upload-video';
 import { createTranscriptionRoute } from './routes/create-transcription';
 import { generateAICompletionRoute } from './routes/generate-ai-completion';
 
-
 const app = fastify();
 
 app.register(fastifyCors, {
